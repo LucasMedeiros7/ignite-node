038c2e07898713008bc563147173cab0afcc84ae
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-09-28 13:31:26 -0300

    configurando eslint

--- node-typescript/src/server.ts
+++ node-typescript/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createCourse } from './routes';
 
 const app = express();
 
-app.get('/', createCourse);
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
