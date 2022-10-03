eecf340a31c31074fdf52cfbbb3fe25686a1620d
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-10-03 16:19:19 -0300

    refatorando listagem de categoria

--- node-typescript/src/routes/categories.routes.ts
+++ node-typescript/src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoryController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request, response) => {
-  const allCategories = categoriesRepository.list()
-
-  return response.json(allCategories)
+  return listCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes }
