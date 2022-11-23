b1e1f09f3d6dd81dc8dc8e3d0d65528abc423eb0
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-11-23 15:15:22 -0300

    config docker

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,13 @@
 import { DataSource } from 'typeorm';
-const dataSource = new DataSource({
+import { Category } from '../modules/cars/model/Category';
+import { Specification } from '../modules/cars/model/Specifications';
+
+export const dataSource = new DataSource({
   type: 'postgres',
+  host: "localhost",
   port: 5432,
   username: 'docker',
   password: 'ignite',
   database: 'rentx',
-  entities: [
-    // Category
-    // ...
-  ],
-  migrations: [
-    // CreateCategories1616082124654
-    // ...
-  ]
+  entities: [Category, Specification],
 });
