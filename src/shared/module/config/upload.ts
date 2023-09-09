9f128f66d046369a5ee4cfa76661bd85f3864245
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2023-09-09 00:37:55 -0300

    fix: update destination path of uploadConfig

--- src/shared/module/config/upload.ts
+++ src/shared/module/config/upload.ts
@@ -6,7 +6,7 @@ export const uploadConfig = {
   upload (folder: string) {
     return {
       storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', folder),
+        destination: resolve(__dirname, '..', '..', '..', '..', folder),
         filename: (_request, file, callback) => {
           const fileHash = randomBytes(16).toString('hex')
           const fileName = `${fileHash}-${file.originalname}`
