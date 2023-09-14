8004cc142cf279299cc14ea84124efaaf542afce
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2023-09-14 11:32:10 -0300

    refactor: using createCarDTO instead CreateCarRequest

--- src/modules/cars/core/useCases/car/CreateCar.useCase.ts
+++ src/modules/cars/core/useCases/car/CreateCar.useCase.ts
@@ -1,31 +1,22 @@
 import { Car } from '@modules/cars/storage/typeorm/models/Car.model'
 import { AppError } from '@shared/errors/AppError'
 import { inject, injectable } from 'tsyringe'
+import { CreateCarDTO } from '../../dtos/CreateCarDTO'
 import { CarsRepository } from '../../repositories/CarsRepository'
-
-interface CreateCarRequest {
-  name: string
-  description: string
-  daily_rate: number
-  license_plate: string
-  fine_amount: number
-  brand: string
-  category_id: string
-}
 
 @injectable()
 export class CreateCarUseCase {
   constructor (
     @inject('CarsRepository')
     private readonly carsRepository: CarsRepository
-  ) {}
+  ) { }
 
-  async execute (createCarPayload: CreateCarRequest): Promise<Car> {
-    const carAlreadyExists = await this.carsRepository.findByLicensePlate(createCarPayload.license_plate)
+  async execute (carData: CreateCarDTO): Promise < Car > {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(carData.license_plate)
     if (carAlreadyExists) {
       throw new AppError('A car with this license plate already exists!')
     }
-    const car = await this.carsRepository.create(createCarPayload)
+    const car = await this.carsRepository.create({ ...carData })
     return car
   }
 }
